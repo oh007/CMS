93af3f54812cb2fd9c57c93a21466deae44fa068
olof <olof.hag123@gmail.com>
2024-06-19 22:36:43 +0200

    Added server

--- cms/src/app/page.js
+++ cms/src/app/page.js
@@ -5,9 +5,10 @@ export default function Home() {
   const [message, setMessage] = useState('');
 
   useEffect(() => {
-    fetch('/api/express')
+    fetch('/api/express/api/hello')
       .then(response => response.json())
-      .then(data => setMessage(data.message));
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching message:', error));
   }, []);
 
   return (
